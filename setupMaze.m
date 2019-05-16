a392c9d4835dc0e932d3b83cac734e335a4471a0
avan4bala <44299374+avan4bala@users.noreply.github.com>
2019-05-16 11:05:36 -0400

    Create setupMaze.m
    This function makes a matrix of certain dimensions using the point class (which will be explained later) and sets up the display of walls inside the maze. It calls 4 parameters: maze, walls, position, and end position and asks for an input of size to out put the 4 parameters. This will make the maze depending on size so it can create a certain number of walls.

---
+++ setupMaze.m
@@ -0,0 +1,18 @@
+%% makes the maze by size input
+maze = zeros(size);
+maze(1, 1:end) = 8;
+maze(1:end, 1) = 8;
+maze(end, 1:end) = 8;
+maze(1:end, end) = 8;
+
+%% CALCULATIONS ---
+%% positions the walls by calling the setMazePosition variable like a coordinate system
+position = point(size, randi([2 (size - 1)]));
+maze = setMazePosition(maze, position, 3);
+position = adjust(position, -1, 0);
+maze = setMazePosition(maze, position, 1);
+wall(1, 1) = position.row;
+wall(2, 1) = position.col;
+
+endPos = point(1, randi([2 (size - 1)]));
+maze = setMazePosition(maze, endPos, 4);
